b6fa676c5b39ded61093ccd309df7e8391f48825
chinanugod <chinanugodwin@gmail.com>
2025-12-12 01:37:15 +0100

    feat: add navigation and smooth scrolling

---
+++ script.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('nav a').forEach(a=>{
+  a.addEventListener('click', e=>{
+    e.preventDefault();
+    const id = a.getAttribute('href');
+    document.querySelector(id).scrollIntoView({behavior:'smooth', block:'start'});
+  });
+});
