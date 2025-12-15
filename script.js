16d08f3948f8dcf978893a9ff00cfb8e28128dee
chinanugod <chinanugodwin@gmail.com>
2025-12-15 22:45:26 +0100

    Add projects section with Chingowin carousal and auto-slide

--- script.js
+++ script.js
@@ -22,3 +22,46 @@ if (form) {
     }
   });
 }
+
+const carousels = document.querySelectorAll('[data-carousel]');
+
+carousels.forEach(carousel => {
+  const images = carousel.querySelectorAll('img');
+  const prevBtn = carousel.querySelector('.prev');
+  const nextBtn = carousel.querySelector('.next');
+
+  let index = 0;
+
+  function showImage(i) {
+    images.forEach(img => img.classList.remove('active'));
+    images[i].classList.add('active');
+  }
+
+  prevBtn.addEventListener('click', () => {
+    index = (index - 1 + images.length) % images.length;
+    showImage(index);
+  });
+
+  nextBtn.addEventListener('click', () => {
+    index = (index + 1) % images.length;
+    showImage(index);
+  });
+
+  setInterval(() => {
+  index = (index + 1) % images.length;
+  showImage(index);
+}, 4000);
+
+let interval = setInterval(nextSlide, 4000);
+
+carousel.addEventListener('mouseenter', () => clearInterval(interval));
+carousel.addEventListener('mouseleave', () => {
+  interval = setInterval(nextSlide, 4000);
+});
+
+function nextSlide() {
+  index = (index + 1) % images.length;
+  showImage(index);
+}
+});
+
